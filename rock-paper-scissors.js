62c1da13340f24860a3ea584ed0ed94140b79bde
Armaan Sharma <armaan192004@gmail.com>
2025-08-26 19:00:18 -0700

    arrow function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -16,7 +16,7 @@ let intervalId;
 
 function autoPlay() {
     if(!isAutoPlaying) {
-        intervalId = setInterval(function() {
+        intervalId = setInterval(() => {
             const playerMove = pickComputerMove();
             makeMove(playerMove);
         }, 1000);
